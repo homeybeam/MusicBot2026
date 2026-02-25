f59874198ac70f7e244bd875192977ebd2a58596
root <root@ChaseBot>
2026-02-24 22:33:10 -0600

    Fix nc syntax for Alpine

--- start.sh
+++ start.sh
@@ -1,5 +1,5 @@
 #!/bin/sh
-# Dummy HTTP listener for Cloud Run health checks
-while true; do echo -e "HTTP/1.1 200 OK\r\n" | nc -l -p 8080; done &
-# Start the bot
+while true; do
+  echo -e "HTTP/1.1 200 OK\r\nContent-Length: 2\r\n\r\nOK" | nc -l 8080
+done &
 java -Xmx512m -jar app.jar
